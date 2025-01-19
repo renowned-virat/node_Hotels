1b2f2dc50f3392d389fae6bef7035ef04ce2f1d6
Shubham Kumar <945skumar@gmail.com>
2025-01-19 15:20:55 +0530

    going for L-7 hosting

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -29,4 +29,37 @@ router.get('/',async(req,res)=>{
     }
 })
 
+router.put('/:id',async(req,res)=>{
+    try {
+        const personId=req.params.id;  //we do send id as a parameter during update
+        const updatedPersonData=req.body;
+        const response=await Person.findByIdAndUpdate(personId,updatedPersonData,{
+            new:true, //returns the updated document
+            runValidators:true,//runs mongoose validation
+        })
+        if(!response){
+            return res.status(404).json({error: "person not found."})
+        }
+
+        console.log('data updated')
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'internal server error'});
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    try {
+        const personId=req.params.id;
+    const response=await Person.findByIdAndDelete(personId);
+    if(!response)return res.status(404).json({error: "person not found"});
+    return res.status(200).json(response);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'internal server error'});
+    }
+    
+})
+
 module.exports=router;
